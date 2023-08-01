0fb6fa26801b00331ef00b65cf699cb1e1730758
David Oyinbo <dev.davexoyinbo@gmail.com>
2023-08-01 11:09:35 +0100

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer, middleware::Logger};
+use env_logger::Env;
+use log::{info, error};
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     HttpServer::new(|| {
-        App::new().route(
+        App::new()
+        .wrap(Logger::default())
+        .route(
             "/",
-            web::get().to(|| async { HttpResponse::Ok().body("This is the response") }),
+            web::get().to(|| async {
+                HttpResponse::Ok().body("This is the response") }),
         )
     })
     .bind(("0.0.0.0", 8080))?
