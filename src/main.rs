9c56b26134d9c1f16626717c9cceae052e1caec2
David Oyinbo <dev.davexoyinbo@gmail.com>
2023-08-01 10:55:15 +0100

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().route(
+            "/",
+            web::get().to(|| async { HttpResponse::Ok().body("This is the response") }),
+        )
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
